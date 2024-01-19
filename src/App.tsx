f4cfa0ae96e7478911d946aab7798f8fd7d94fce
pedroorf <pedroorf@gmail.com>
2024-01-19 15:33:35 -0300

    fisrt

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Typescript Webpack Starter Template</h1>
+    </div>
+  );
+}
+
+export default App;
